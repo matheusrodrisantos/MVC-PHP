73870d99e9a8aeb377105561b65cfdd19f4456e9
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2018-02-07 01:58:27 -0200

    Arquivos base
    Adicionando os arquivos base para MVC em PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    // Requisito o uso do autoloader
+    require 'lib/autoloader.php';
+    // Coloco o encodamento interno das paginas para UTF-8
+    mb_internal_encoding("UTF-8");
+    // Definindo a timezone do PHP
+    date_default_timezone_set('America/Sao_Paulo');
+    // Conexao com o banco de dados
+    //    try {
+    //        Db::connect('host', 'usuario', 'senha', 'base');
+    //    } catch (Exception $e) {
+    //        // Mensagem de erro caso não consiga conectar ao banco
+    //        die("Erro ao conectar ao banco de dados!");
+    //    }
+    // Criar um objeto instanciado da classe RouterController, e passar via superglobal $_SERVER
+    // um array da url requisitada via REQUEST_URI para que o Router processe essa informação.
+    $router = new RouterController();
+    $router->process(array($_SERVER['REQUEST_URI']));
+    $router->renderView();
+?>
